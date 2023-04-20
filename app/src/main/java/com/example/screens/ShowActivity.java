d330a05908723af77cd2089caeba79130495c549
a22danwe <a22danwe@student.his.se>
2023-04-20 11:56:13 +0200

    fixat variablarna

--- app/src/main/java/com/example/screens/ShowActivity.java
+++ app/src/main/java/com/example/screens/ShowActivity.java
@@ -3,12 +3,24 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowActivity extends AppCompatActivity {
+
+   TextView TextViewName;
+   TextView TextViewAge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+        TextViewName=findViewById(R.id.textViewName);
+        TextViewAge=findViewById(R.id.textViewAge);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            int number = extras.getInt("age");
+            // Do something with the name and number
+        }
     }
 }
