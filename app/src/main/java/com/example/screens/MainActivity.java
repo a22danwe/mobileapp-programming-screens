3b99c9b294dd098ce141e8346c9f01dcf5475535
a22danwe <a22danwe@student.his.se>
2023-04-20 11:19:53 +0200

    Skapat onClicklistener för button

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button pressButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pressButton = findViewById(R.id.pressButton);
+        pressButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShowActivity.class);
+                intent.putExtra("name", "Daniel"); // Optional
+                intent.putExtra("Age", 1); // Optional
+                startActivity(intent);
+            }
+        });
     }
 }
