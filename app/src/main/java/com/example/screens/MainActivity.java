8e7f13ce381747d428495853622318223270c034
a22danwe <a22danwe@student.his.se>
2023-04-20 11:52:27 +0200

    lagt till variablar kopplade till det som ska till nästa sida.

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText EditTextName;
+    EditText EditTextAge;
     Button pressButton;
 
     @Override
@@ -16,13 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pressButton = findViewById(R.id.pressButton);
+        EditTextName=findViewById(R.id.editTextTextName);
+        EditTextAge=findViewById(R.id.editTextTextAge);
         pressButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ShowActivity.class);
-                intent.putExtra("name", "Daniel"); // Optional
-                intent.putExtra("Age", 1); // Optional
+                intent.putExtra("name", EditTextName.getText().toString()); // Optional
+                intent.putExtra("Age", EditTextAge.getText().toString()); // Optional
                 startActivity(intent);
+
+
+
             }
         });
     }
